d8847aa9346bc715343ee14613ae776a3b28f4e7
watashiwaaniket <aniketdhakane3@gmail.com>
2025-09-27 00:38:16 +0530

    added product fetching and product card, will implement dynamic displaying of card and cart feature

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+export interface ProductCardProps{
+    name : string;
+    price : string;
+    imageUrl : string;
+}
+
+export default function ProductCard({name, price, imageUrl} : ProductCardProps) {
+    return(
+        <div
+            className="border border-dashed border-neutral-400 min-h-96 rounded-xl bg-neutral-100"
+        >
+            <Image 
+                src={imageUrl} 
+                width={300} 
+                height={300} 
+                alt="anc"
+                className="rounded-2xl m-2 border border-neutral-300" 
+            />
+            <div className="flex justify-between items-center px-4">
+                <div>
+                    <p className="font-semibold text-lg">{name}</p>
+                    <p>₹{price}</p>
+                </div>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 text-pink-400 hover:cursor-pointer">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                    </svg>
+                </div>
+            </div>
+            
+        </div>
+    )
+};
--- app/components/sections/Products.tsx
+++ app/components/sections/Products.tsx
@@ -1,12 +1,33 @@
-export default function Products() {
+import axios from "axios";
+import ProductCard from "../ProductCard";
+
+export interface ProductSchema{
+  id : string;
+  name : string;
+  price : string;
+  imageUrl : string
+}
+
+async function fetchProducts() : Promise<ProductSchema[]> {
+  const response = await axios.get<ProductSchema[]>("http://localhost:3000/products")
+  return response.data;
+}
+
+export default async function Products() {
+    const products : ProductSchema[] = await fetchProducts();
+
     return(
         <section className="w-screen flex flex-col items-center -mt-26 relative z-40">
             <div className="w-screen bg-neutral-50 p-2 overflow-clip h-9.5">
                 FLASH SALE * 10TH OCT * FLASH SALE * 10TH OCT * FLASH SALE * 10TH OCT * FLASH SALE * 10TH OCT * FLASH SALE * 10TH OCT * FLASH SALE * 10TH OCT * FLASH SALE * 10TH OCT * FLASH SALE * 10TH OCT * FLASH SALE * 10TH OCT *
             </div>
-            <div className="w-96 px-4 md:px-6 sm:w-[620px] md:w-[860px] lg:w-[1200px] bg-neutral-50 rounded-lg h-96 mt-4">
-                products
+            <div className="w-96 p-4 md:p-6 sm:w-[620px] md:w-[860px] lg:w-[1200px] bg-neutral-50 rounded-lg mt-4 flex flex-wrap">
+                <ProductCard 
+                    name={products[0].name}
+                    price={products[0].price}
+                    imageUrl={products[0].imageUrl}
+                />
             </div>
-        </section>
+        </section> 
     )
 };
