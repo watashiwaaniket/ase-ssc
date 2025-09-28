0b376745eeb24d7127e2e43da6c0fd1774fa3584
watashiwaaniket <aniketdhakane3@gmail.com>
2025-09-28 18:07:21 +0530

    finished with the project

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,6 +3,11 @@ import axios from "axios";
 import { CartItem, ProductSchema } from "../utils/interfaces"
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { Button } from "../components/Button";
+import { useRouter } from "next/navigation";
+import { useCartNavContext } from "../utils/CartContext";
+import Link from "next/link";
+
 
 async function fetchProducts() : Promise<ProductSchema[]> {
   const response = await axios.get<ProductSchema[]>("http://localhost:3000/products")
@@ -13,6 +18,8 @@ export default function page() {
     const [products, setProducts] = useState<ProductSchema[]>([]);
     const [cartItems, setCartItems] = useState<CartItem[]>([]);
     const [totalCost, setTotalCost] = useState(0);
+    const router = useRouter();
+    const {setCartBadge} = useCartNavContext();
 
     useEffect(() => {
         fetchProducts().then(
@@ -78,16 +85,33 @@ export default function page() {
         }
     ).filter((item) => item !== null);
 
+    async function completeOrder(){
+        
+        console.log(cartItems)
+        const response = await axios.post("http://localhost:3000/products", cartItems)
+        if(response.status === 201){
+            console.log(response.data)
+
+            //clearing the localstorage
+            localStorage.clear();
+            setCartBadge(0);
+            router.push('http://localhost:3000');
+        }else{
+            alert('Order did not complete');
+        }
+    }
+
     return(
         <section className="bg-neutral-50 w-screen h-screen flex flex-col items-center">
-            <h1 className="bg-neutral-950 text-neutral-50 w-screen text-center p-4 font-medium">
+            <h1 className="bg-amber-100 text-neutral-950 w-80 md:w-[680px] text-center p-4 font-medium border-x border-neutral-400 border-dashed border-b flex gap-2 items-center justify-center">
+                <Link href={'/'}>{`<`}</Link>
                 Cart
             </h1>
-            <div className="p-4">
+            <div className="">
                 {cartWithProducts.length > 0 ? (
-                    <ul className="flex flex-col gap-1.5 w-80 md:w-[680px]">
+                    <ul className="flex flex-col w-80 md:w-[680px]">
                     {cartWithProducts.map((item) => (
-                        <li key={item.id} className="bg-white border border-neutral-300 rounded-sm flex gap-2 md:gap-4 justify-between">
+                        <li key={item.id} className="bg-white border-x border-b border-neutral-400 border-dashed flex gap-2 md:gap-4 justify-between py-4">
                             <div className="flex gap-2 md:gap-4">
                                 <Image
                                     src={item.imageUrl}
@@ -110,7 +134,7 @@ export default function page() {
                                         />
                                     </p>
                                     <p>
-                                        Price: ${parseFloat(item.price) * item.quantity}
+                                        Price: ₹{parseFloat(item.price) * item.quantity}
                                     </p>
                                 </div>
                             </div>
@@ -121,8 +145,18 @@ export default function page() {
                     <p>Your cart is empty.</p>
                 )}
             </div>
-            <div>
-                Total : {totalCost}
+            <div className="w-80 md:w-[680px] border-x border-neutral-400 border-dashed h-screen bg-white p-2 md:p-4">
+                <div className="flex  items-center justify-between">
+                    <h2 className="text-lg font-medium">Total : ₹{totalCost}</h2>
+                    <div>
+                        <Button
+                            label="Complete Order"
+                            type="order"
+                            onClick={completeOrder}
+                            totalCost={totalCost}
+                        />
+                    </div>
+                </div>
             </div>
         </section>
     )
--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,15 +1,21 @@
 export interface ButtonProps{
     label : string;
-    type : "primary" | "secondary";
+    type : "primary" | "secondary" | "order";
+    onClick ?: () => void;
+    totalCost ?: number;
 }
 
-export const Button = ({label, type} : ButtonProps) => {
+export const Button = ({label, type, onClick, totalCost} : ButtonProps) => {
     return(
         <button
-            className={` text-xl font-medium px-4 py-2 rounded-sm cursor-pointer
-                ${type == "primary" ? 'bg-neutral-50 text-black hover:bg-neutral-200' : ''}
-                ${type == "secondary" ? '' : ''}
+            className={`font-medium px-4 py-2 
+                ${type === "primary" ? 'bg-neutral-50 text-black hover:bg-neutral-200 rounded-sm text-xl cursor-pointer' : ''}
+                ${type === "secondary" ? 'text-xl cursor-pointer' : ''}
+                ${type === "order" ? 'border border-dashed bg-neutral-500 text-white' : ''}
+                ${totalCost === 0 ? 'cursor-not-allowed' : ''}
             `}
+            disabled={totalCost === 0 ? true : false}
+            onClick={onClick}
         >
             {label}
         </button>
